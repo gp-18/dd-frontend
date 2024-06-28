639117953e6c03c23553279bb8efba7192b34fc6
Zignuts Parth Gupta <parthg@zignuts.com>
2024-06-28 15:36:44 +0530

    functionality added : view and download button working in important information

--- src/components/ImportantInfoDialog .jsx
+++ src/components/ImportantInfoDialog .jsx
@@ -2,16 +2,34 @@ import React from 'react'
 import { Dialog, DialogActions, DialogContent, DialogTitle, Button, Typography } from '@mui/material'
 
 const ImportantInfoDialog = ({ open, handleClose }) => {
+  // Using the environment variable for the link, uncomment if you prefer using it
+  // const link = process.env.REACT_APP_BACKEND_STORAGE_PATH;
+
+  // For now, using a hardcoded link
+  const link = 'http://127.0.0.1:8000/storage/'
   const files = [
-    { name: 'Users With Managers Excel File', url: "ExcelFiles/users-with-managers" },
-    { name: 'Users With Incentives Excel File', url: "ExcelFiles/users-with-incentives" }
+    {
+      name: 'Users With Managers Excel File',
+      url: `${link}users-with-managers.xlsx`,
+      openUrl: `${link}userManagers.png`
+    },
+    {
+      name: 'Users With Incentives Excel File',
+      url: `${link}users-with-incentives.xlsx`,
+      openUrl: `${link}userIncentive.png`
+    },
+    {
+      name: 'Naming Conventions In Mails',
+      url: `${link}naming-convention-in-mail.docx`,
+      openUrl: `${link}namingConvention.png`
+    }
   ]
 
-  const handleView = (url) => {
-    window.open(url, '_blank')
+  const handleView = openUrl => {
+    window.open(openUrl, '_blank')
   }
 
-  const handleDownload = (url) => {
+  const handleDownload = url => {
     const link = document.createElement('a')
     link.href = url
     link.download = url.split('/').pop()
@@ -19,38 +37,29 @@ const ImportantInfoDialog = ({ open, handleClose }) => {
   }
 
   return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="important-info-dialog-title">
-      <DialogTitle id="important-info-dialog-title">Important Information</DialogTitle>
+    <Dialog open={open} onClose={handleClose} aria-labelledby='important-info-dialog-title'>
+      <DialogTitle id='important-info-dialog-title'>Important Information</DialogTitle>
       <DialogContent>
-        <Typography variant="body1">
-          Points that needs to be taken care of 
+        <Typography variant='body1' style={{ color: 'red' }}>
+          Points that needs to be taken care of
         </Typography>
-        <Typography variant="h6" sx={{ mt: 2 }}>
-          Files:
+        <Typography variant='h6' sx={{ mt: 2 }}>
+          Files
         </Typography>
         {files.map((file, index) => (
           <div key={index} style={{ marginTop: '10px' }}>
-            <Typography variant="body1">{file.name}</Typography>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => handleView(file.url)}
-              sx={{ mr: 1 }}
-            >
+            <Typography variant='body1'>{file.name}</Typography>
+            <Button variant='contained' color='primary' onClick={() => handleView(file.openUrl)} sx={{ mr: 1 }}>
               View
             </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => handleDownload(file.url)}
-            >
+            <Button variant='outlined' color='success' onClick={() => handleDownload(file.url)}>
               Download
             </Button>
           </div>
         ))}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button onClick={handleClose} color='error'>
           Close
         </Button>
       </DialogActions>
